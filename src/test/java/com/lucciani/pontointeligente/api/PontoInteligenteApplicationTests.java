74ccee1f0adf1e69a76b28602acd0b3f3d2e20a4
glucciani <giancarlolucciani@gmail.com>
2020-06-19 20:19:39 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/lucciani/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucciani.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
